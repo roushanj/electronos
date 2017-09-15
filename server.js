8571e7c7a8a9a90768f85975f064456c611f8bdd
Roushan <roushan.jha0709@gmail.com>
2017-09-15 18:04:45 +0530

    added a server

---
+++ server.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', function () {
+	
+	mainWindow = new BrowserWindow({});
+
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'mainWindow.html'),
+		protocol:'file:',
+		slashes: true
+	}));
+
+
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+	Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+  {
+  	label:'File'
+  	
+  }
+];
